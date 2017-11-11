33c2f5b210167b53c94ccc65ed49757c88e853b0
Nicolas Phister <nicolas@phister.fr>
2017-11-11 23:28:06 +0100

    made KinematicObject take its boundingBox as parameter

--- common/gameobjects/KinematicObject.hpp
+++ common/gameobjects/KinematicObject.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "GameObject.hpp"
+#include "common/components/TransformComponent.hpp"
 #include "common/components/PhysicsComponent.hpp"
 
 namespace kengine
@@ -8,10 +9,11 @@ namespace kengine
     class KinematicObject : public GameObject, public putils::Reflectible<KinematicObject>
     {
     public:
-        KinematicObject(std::string_view name, bool solid = true, bool fixed = false)
+        KinematicObject(std::string_view name, const putils::Rect3d &box = { { 0, 0, 0 }, { 1, 1, 1 } },
+                        bool solid = true, bool fixed = false)
                 : GameObject(name)
         {
-            attachComponent<kengine::TransformComponent3d>();
+            attachComponent<kengine::TransformComponent3d>(box);
             attachComponent<kengine::PhysicsComponent>(solid, fixed);
         }
 
