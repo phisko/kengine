9e1df4e3dac09baaa0272b0181dc22aa2504e08a
Nicolas Phister <nicolas@phister.fr>
2017-11-13 09:40:06 +0100

    fixed repetition in reflection in Meta and Pathfinder components

--- common/components/PathfinderComponent.hpp
+++ common/components/PathfinderComponent.hpp
@@ -8,6 +8,7 @@ namespace kengine
     class PathfinderComponent : public kengine::SerializableComponent<PathfinderComponent>
     {
     public:
+        const std::string type = pmeta_nameof(PathfinderComponent);
         putils::Point3d dest;
         bool reached = true;
         bool diagonals = true;
@@ -18,29 +19,18 @@ namespace kengine
          * Reflectible
          */
     public:
-        static const auto get_class_name() { return "PathfinderComponent"; }
+        static const auto get_class_name() { return pmeta_nameof(PathfinderComponent); }
 
         static const auto &get_attributes()
         {
             static const auto table = pmeta::make_table(
+                    pmeta_reflectible_attribute(&PathfinderComponent::type),
                     pmeta_reflectible_attribute(&PathfinderComponent::dest),
                     pmeta_reflectible_attribute(&PathfinderComponent::reached),
                     pmeta_reflectible_attribute(&PathfinderComponent::diagonals),
                     pmeta_reflectible_attribute(&PathfinderComponent::desiredDistance),
                     pmeta_reflectible_attribute(&PathfinderComponent::maxAvoidance)
             );
-            return table;
-        }
-
-        static const auto &get_methods()
-        {
-            static const auto table = pmeta::make_table();
-            return table;
-        }
-
-        static const auto &get_parents()
-        {
-            static const auto table = pmeta::make_table();
             return table;
         }
     };
