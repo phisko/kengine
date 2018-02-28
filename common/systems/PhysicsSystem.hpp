ad926d1e51f5f4e6436244b34c323c1eb7b8ff33
Nicolas Phister <nicolas@phister.fr>
2018-02-28 18:40:07 +0100

    made PhysicsSystem not rely on QuadTree anymore

--- common/systems/PhysicsSystem.hpp
+++ common/systems/PhysicsSystem.hpp
@@ -6,23 +6,11 @@
 #include "common/components/TransformComponent.hpp"
 #include "common/packets/Position.hpp"
 #include "common/packets/Collision.hpp"
-#include "QuadTree.hpp"
 
 namespace kengine {
-    namespace packets {
-        struct QuadTreeQuery {
-            putils::BaseModule * sender;
-        };
-    }
-
-    class PhysicsSystem : public kengine::System<PhysicsSystem,
-            kengine::packets::RegisterGameObject, kengine::packets::RemoveGameObject,
-            packets::Position::Query, packets::QuadTreeQuery> {
+    class PhysicsSystem : public kengine::System<PhysicsSystem, packets::Position::Query> {
     public:
-        PhysicsSystem(kengine::EntityManager & em)
-                : _tree({ { 0,  0 },
-                          { 64, 64 } }),
-                  _em(em) {}
+        PhysicsSystem(kengine::EntityManager & em) : _em(em) {}
 
     public:
         void execute() final {
@@ -30,40 +18,17 @@ namespace kengine {
                 updatePosition(*go);
         }
 
-        void handle(const kengine::packets::RegisterGameObject & p) noexcept {
-            auto & go = p.go;
-            if (go.hasComponent<kengine::TransformComponent3d>() && go.hasComponent<kengine::PhysicsComponent>() &&
-                go.getComponent<kengine::PhysicsComponent>().solid) {
-                const auto & box = go.getComponent<kengine::TransformComponent3d>().boundingBox;
-                _tree.add(&go, { { box.topLeft.x, box.topLeft.z },
-                                 { box.size.x,    box.size.z } });
-            }
-        }
-
-        void handle(const kengine::packets::RemoveGameObject & p) noexcept {
-            auto & go = p.go;
-            if (go.hasComponent<kengine::TransformComponent3d>() && go.hasComponent<kengine::PhysicsComponent>() &&
-                go.getComponent<kengine::PhysicsComponent>().solid)
-                _tree.remove(&go);
-        }
-
     public:
         void handle(const packets::Position::Query & q) {
-            sendTo(
-                    packets::Position::Response {
-                            _tree.query(
-                                    {
-                                            { q.box.topLeft.x, q.box.topLeft.z },
-                                            { q.box.size.x,    q.box.size.z }
-                                    }
-                            )
-                    },
-                    *q.sender
-            );
-        }
+            std::vector<kengine::GameObject *> found;
 
-        void handle(const packets::QuadTreeQuery & q) {
-            sendTo(&_tree, *q.sender);
+            for (const auto go : _em.getGameObjects<kengine::PhysicsComponent>()) {
+                auto & box = go->getComponent<kengine::TransformComponent3d>().boundingBox;
+                if (box.intersect(q.box))
+                    found.push_back(go);
+            }
+
+            sendTo( packets::Position::Response { found }, *q.sender);
         }
 
         // Helpers
@@ -78,15 +43,10 @@ namespace kengine {
             const auto dest = getNewPos(box.topLeft, phys.movement, phys.speed);
             if (dest == box.topLeft)
                 return;
-
-            const auto pos = putils::Rect2d{ { dest.x,     dest.z },
-                                             { box.size.x, box.size.z } };
             box.topLeft = dest;
 
-            if (phys.solid) {
-                _tree.move(&go, pos);
-                checkCollisions(go, pos);
-            }
+            if (phys.solid)
+                checkCollisions(go, box);
         }
 
         putils::Point3d getNewPos(const putils::Point3d & pos, const putils::Point3d & movement, double speed) {
@@ -97,16 +57,18 @@ namespace kengine {
             };
         }
 
-        void checkCollisions(kengine::GameObject & go, const putils::Rect2d & box) {
-            const auto objects = _tree.query(box);
+        void checkCollisions(kengine::GameObject & go, const putils::Rect3d & box) {
+            for (const auto obj : _em.getGameObjects<kengine::PhysicsComponent>()) {
+                if (obj == &go)
+                    continue;
 
-            for (const auto obj : objects)
-                if (obj != &go)
+                const auto & other = obj->getComponent<kengine::TransformComponent3d>().boundingBox;
+                if (box.intersect(other))
                     send(kengine::packets::Collision{ go, *obj });
+            }
         }
 
     private:
         kengine::EntityManager & _em;
-        putils::QuadTree<kengine::GameObject *, double> _tree;
     };
 }
