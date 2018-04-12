8d014cbddc7b3f939a577caa624bcd827b690354
Nicolas Phister <nicolas@phister.fr>
2018-04-12 14:07:24 +0200

    added CollisionComponent::type and InputCollision::type + putils update

--- common/components/InputComponent.hpp
+++ common/components/InputComponent.hpp
@@ -4,18 +4,15 @@
 #include "SerializableComponent.hpp"
 
 namespace kengine {
-    class InputComponent : public SerializableComponent<InputComponent> {
-    public:
-        InputComponent() = default;
-
-    public:
+    struct InputComponent : SerializableComponent<InputComponent> {
+		const std::string type = pmeta_nameof(InputComponent);
         std::function<void(int keycode, bool pressed)> onKey = nullptr;
         std::function<void(int x, int y)> onMouseMove = nullptr;
         std::function<void(int button, int x, int y, bool pressed)> onMouseButton = nullptr;
 
-    public:
         pmeta_get_class_name(InputComponent);
         pmeta_get_attributes(
+            pmeta_reflectible_attribute(&InputComponent::type),
             pmeta_reflectible_attribute(&InputComponent::onKey),
             pmeta_reflectible_attribute(&InputComponent::onMouseMove),
             pmeta_reflectible_attribute(&InputComponent::onMouseButton)
