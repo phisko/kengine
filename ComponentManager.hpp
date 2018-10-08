da3f6ed523bea12d7bd43c2d5bf504ae9a3c048e
Nicolas Phister <nicolas@phister.fr>
2018-10-08 11:43:13 +0200

    avoided systematic vector copies in ComponentManager

--- ComponentManager.hpp
+++ ComponentManager.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "with.hpp"
 #include "Component.hpp"
 
 namespace kengine {
@@ -26,9 +27,10 @@ namespace kengine {
         const std::vector<GameObject *> & getGameObjects() const noexcept { return _allEntities.safe; }
 
 		void updateEntitiesByType() noexcept {
-			_allEntities.safe = _allEntities.unsafe;
+			_allEntities.update();
+
 			for (auto & [type, category] : _entitiesByType)
-				category.safe = category.unsafe;
+				category.update();
         }
 
     protected:
@@ -37,35 +39,50 @@ namespace kengine {
             for (auto & [type, comp] : go._components)
                 registerComponent(go, *comp);
             _allEntities.unsafe.push_back(&go);
+			_allEntities.changed = true;
         }
 
         void removeGameObject(const GameObject & go) noexcept {
             for (const auto type : go._types)
                 removeComponent(go, type);
 			const auto it = std::find(_allEntities.unsafe.begin(), _allEntities.unsafe.end(), &go);
-			if (it != _allEntities.unsafe.end())
+			if (it != _allEntities.unsafe.end()) {
 				_allEntities.unsafe.erase(it);
+				_allEntities.changed = true;
+			}
         }
 
     private:
         friend class GameObject;
 
         void registerComponent(GameObject & parent, const IComponent & comp) noexcept {
-			_entitiesByType[comp.getType()].unsafe.push_back(&parent);
+			auto & category = _entitiesByType[comp.getType()];
+			category.unsafe.push_back(&parent);
+			category.changed = true;
         }
 
 		void removeComponent(const GameObject & go, pmeta::type_index type) noexcept {
 			auto & category = _entitiesByType[type];
 			const auto it = std::find(category.unsafe.begin(), category.unsafe.end(), &go);
-			if (it != category.unsafe.end())
+			if (it != category.unsafe.end()) {
 				category.unsafe.erase(it);
+				category.changed = true;
+			}
 		}
 
     private:
 		struct EntityCollection
 		{
+			bool changed = false;
 			std::vector<GameObject *> safe;
 			std::vector<GameObject *> unsafe;
+
+			void update() {
+				if (!changed)
+					return;
+				safe = unsafe;
+				changed = false;
+			}
 		};
         std::unordered_map<pmeta::type_index, EntityCollection> _entitiesByType;
         EntityCollection _allEntities;
