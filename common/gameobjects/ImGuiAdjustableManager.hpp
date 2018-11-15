97abd0d9c35f5c26d558d2bb908a5319e796d6da
Nicolas Phister <nicolas@phister.fr>
2018-11-15 15:05:57 +0100

    added save option to imgui adjustable manager

--- common/gameobjects/ImGuiAdjustableManager.hpp
+++ common/gameobjects/ImGuiAdjustableManager.hpp
@@ -8,6 +8,92 @@
 namespace kengine {
 
 	namespace detail {
+		static constexpr auto saveFile = "adjust.cnf";
+		static constexpr auto separator = ';';
+
+		static void save(kengine::EntityManager & em) {
+			std::ofstream f(saveFile, std::ofstream::trunc);
+
+			for (const auto & [e, comp] : em.getEntities<AdjustableComponent>()) {
+				f << comp.name << separator;
+				switch (comp.adjustableType) {
+				case AdjustableComponent::String:
+					f << comp.s;
+					break;
+				case AdjustableComponent::Int:
+					f << comp.i;
+					break;
+				case AdjustableComponent::Double:
+					f << comp.d;
+					break;
+				case AdjustableComponent::Bool:
+					f << std::boolalpha << comp.b << std::noboolalpha;
+					break;
+				default:
+					assert("Unknown adjustable type" && false);
+				}
+				f << '\n';
+			}
+		}
+
+		static void load(kengine::EntityManager & em) {
+			std::unordered_map<std::string, std::string> lines; {
+				std::ifstream f(saveFile);
+				for (std::string line; std::getline(f, line);) {
+					const auto index = line.find(separator);
+					lines[line.substr(0, index)] = line.substr(index + 1);
+				}
+			}
+
+			for (auto & [e, comp] : em.getEntities<AdjustableComponent>()) {
+				const auto it = lines.find(comp.name);
+				if (it == lines.end())
+					continue;
+
+				const auto & val = it->second;
+
+				const auto assignPtr = [](auto ptr, const auto & val) {
+					if (ptr != nullptr)
+						*ptr = val;
+				};
+
+				switch (comp.adjustableType) {
+				case AdjustableComponent::String:
+					comp.s = val;
+					assignPtr(comp.sPtr, comp.s);
+					break;
+				case AdjustableComponent::Int:
+					comp.i = putils::parse<int>(val);
+					assignPtr(comp.iPtr, comp.i);
+					break;
+				case AdjustableComponent::Double:
+					comp.d = putils::parse<float>(val);
+					assignPtr(comp.dPtr, comp.d);
+					break;
+				case AdjustableComponent::Bool:
+					comp.b = putils::parse<bool>(val);
+					assignPtr(comp.bPtr, comp.b);
+					break;
+				default:
+					assert("Unknown adjustable type" && false);
+				}
+			}
+		}
+
+		static std::pair<std::string, std::string> getNameAndSection(const std::string & fullName) {
+			std::pair<std::string, std::string> ret;
+
+			const auto sectionEnd = fullName.find(']');
+			if (fullName[0] == '[' && sectionEnd != std::string::npos) {
+				ret.first = fullName.substr(sectionEnd + 1);
+				ret.second = fullName.substr(1, sectionEnd - 1);
+			}
+			else
+				ret.first = fullName;
+
+			return ret;
+		}
+
 		static std::vector<std::string> split(const std::string & s, char delim) {
 			std::vector<std::string> ret;
 
@@ -35,6 +121,87 @@ namespace kengine {
 
 			return ret;
 		}
+
+		static size_t updateImGuiTree(bool & hidden, const std::vector<std::string> & subs, const std::vector<std::string> & previousSubsections) {
+			auto current = previousSubsections.size() - 1;
+
+			if (!previousSubsections.empty()) {
+				while (current >= subs.size()) {
+					if (!hidden)
+						ImGui::TreePop();
+					hidden = false;
+					--current;
+				}
+
+				while (subs[current] != previousSubsections[current]) {
+					if (current == 0) {
+						if (!hidden)
+							ImGui::TreePop();
+						hidden = false;
+						--current;
+						break;
+					}
+					if (!hidden)
+						ImGui::TreePop();
+					hidden = false;
+					--current;
+				}
+			}
+
+			if (!hidden) {
+				++current;
+				while (current < subs.size()) {
+					if (!ImGui::TreeNodeEx(subs[current].c_str())) {
+						hidden = true;
+						break;
+					}
+					++current;
+				}
+			}
+
+			return current;
+		}
+
+		static void draw(const std::string & name, AdjustableComponent & comp) {
+			ImGui::Columns(2);
+			ImGui::Text(name.c_str());
+			ImGui::NextColumn();
+
+			switch (comp.adjustableType) {
+			case AdjustableComponent::String: {
+				char buff[1024];
+				const auto ret = comp.s.copy(buff, sizeof(buff));
+				buff[ret] = 0;
+
+				ImGui::InputText(putils::concat("##", comp.name).c_str(), buff, sizeof(buff));
+				comp.s = buff;
+				if (comp.sPtr != nullptr)
+					*comp.sPtr = comp.s;
+				break;
+			}
+			case AdjustableComponent::Bool: {
+				ImGui::Checkbox(putils::concat("##", comp.name).c_str(), &comp.b);
+				if (comp.bPtr != nullptr)
+					*comp.bPtr = comp.b;
+				break;
+			}
+			case AdjustableComponent::Double: {
+				ImGui::InputFloat(putils::concat("##", comp.name).c_str(), &comp.d);
+				if (comp.dPtr != nullptr)
+					*comp.dPtr = comp.d;
+				break;
+			}
+			case AdjustableComponent::Int: {
+				ImGui::InputInt(putils::concat("##", comp.name).c_str(), &comp.i);
+				if (comp.iPtr != nullptr)
+					*comp.iPtr = comp.i;
+				break;
+			}
+			default:
+				assert("Unknown type" && false);
+			}
+			ImGui::Columns();
+		}
 	}
 
 	static auto ImGuiAdjustableManager(EntityManager & em) {
@@ -45,123 +212,54 @@ namespace kengine {
 					ImGui::InputText("Name", nameSearch, sizeof(nameSearch));
 					ImGui::Separator();
 
-					std::vector<AdjustableComponent *> comps;
-
-					for (const auto &[e, _] : em.getEntities<AdjustableComponent>()) {
-						if (_.name.find(nameSearch) == std::string::npos)
-							continue;
-						comps.emplace_back(&_);
+					ImGui::Columns(2);
+					if (ImGui::Button("Save")) {
+						detail::save(em);
 					}
-					std::sort(comps.begin(), comps.end(), [](const auto lhs, const auto rhs) {
-						return strcmp(lhs->name.c_str(), rhs->name.c_str()) < 0; 
-					});
-
-					std::vector<std::string> previousSubsections;
-					std::string previousSection;
-					bool hidden = false;
-					for (const auto _ : comps) {
-						std::string name;
-						std::string section; {
-							const auto sectionEnd = _->name.find(']');
-							if (_->name[0] == '[' && sectionEnd != std::string::npos) {
-								section = _->name.substr(1, sectionEnd - 1);
-								name = _->name.substr(sectionEnd + 1);
-							}
-							else
-								name = _->name;
-						}
-
-						if (section != previousSection) {
-							const auto subs = detail::split(section, '/');
-
-							auto current = previousSubsections.size() - 1;
-							if (!previousSubsections.empty()) {
-								while (current >= subs.size()) {
-									if (!hidden)
-										ImGui::TreePop();
-									hidden = false;
-									--current;
-								}
-
-								while (subs[current] != previousSubsections[current]) {
-									if (current == 0) {
-										if (!hidden)
-											ImGui::TreePop();
-										hidden = false;
-										--current;
-										break;
-									}
-									if (!hidden)
-										ImGui::TreePop();
-									hidden = false;
-									--current;
-								}
-							}
-
-							// if (hidden && current != std::numeric_limits<decltype(current)>::max() && subs[current] == previousSubsections[current])
-							// 	continue;
-
-							if (!hidden) {
-								++current;
-								while (current < subs.size()) {
-									if (!ImGui::TreeNodeEx(subs[current].c_str())) {
-										hidden = true;
-										break;
-									}
-									++current;
-								}
-							}
-
-							previousSubsections = std::move(subs);
-							if (current < previousSubsections.size())
-								previousSubsections.erase(previousSubsections.begin() + current + 1, previousSubsections.end());
-							previousSection = detail::reconstitutePath(previousSubsections);
-						}
-
-						if (hidden)
-							continue;
-
-						ImGui::Columns(2);
-						ImGui::Text(name.c_str());
-						ImGui::NextColumn();
-
-						switch (_->adjustableType) {
-						case AdjustableComponent::String: {
-							char buff[1024];
-							const auto ret = _->s.copy(buff, sizeof(buff));
-							buff[ret] = 0;
-
-							ImGui::InputText(putils::concat("##", _->name).c_str(), buff, sizeof(buff));
-							_->s = buff;
-							if (_->sPtr != nullptr)
-								*_->sPtr = _->s;
-							break;
-						}
-						case AdjustableComponent::Bool: {
-							ImGui::Checkbox(putils::concat("##", _->name).c_str(), &_->b);
-							if (_->bPtr != nullptr)
-								*_->bPtr = _->b;
-							break;
-						}
-						case AdjustableComponent::Double: {
-							ImGui::InputFloat(putils::concat("##", _->name).c_str(), &_->d);
-							if (_->dPtr != nullptr)
-								*_->dPtr = _->d;
-							break;
-						}
-						case AdjustableComponent::Int: {
-							ImGui::InputInt(putils::concat("##", _->name).c_str(), &_->i);
-							if (_->iPtr != nullptr)
-								*_->iPtr = _->i;
-							break;
-						}
-						default:
-							assert("Unknown type" && false);
-						}
-						ImGui::Columns();
+					ImGui::NextColumn();
+					if (ImGui::Button("Load")) {
+						detail::load(em);
 					}
-					for (size_t i = hidden ? 1 : 0; i < previousSubsections.size(); ++i)
-						ImGui::TreePop();
+					ImGui::Columns();
+
+					if (ImGui::BeginChild("##adjustables")) {
+						std::vector<AdjustableComponent *> comps;
+
+						for (const auto &[e, _] : em.getEntities<AdjustableComponent>()) {
+							if (_.name.find(nameSearch) == std::string::npos)
+								continue;
+							comps.emplace_back(&_);
+						}
+						std::sort(comps.begin(), comps.end(), [](const auto lhs, const auto rhs) {
+							return strcmp(lhs->name.c_str(), rhs->name.c_str()) < 0;
+						});
+
+						std::vector<std::string> previousSubsections;
+						std::string previousSection;
+						bool hidden = false;
+						for (const auto comp : comps) {
+							const auto[name, section] = detail::getNameAndSection(comp->name);
+
+							if (section != previousSection) {
+								const auto subs = detail::split(section, '/');
+
+								const auto current = detail::updateImGuiTree(hidden, subs, previousSubsections);
+
+								previousSubsections = std::move(subs);
+								if (current < previousSubsections.size())
+									previousSubsections.erase(previousSubsections.begin() + current + 1, previousSubsections.end());
+								previousSection = detail::reconstitutePath(previousSubsections);
+							}
+
+							if (hidden)
+								continue;
+
+							detail::draw(name, *comp);
+						}
+						for (size_t i = hidden ? 1 : 0; i < previousSubsections.size(); ++i)
+							ImGui::TreePop();
+					}
+					ImGui::EndChild();
 				}
 				ImGui::End();
 			});
