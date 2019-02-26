8defe3910b28efc11d3745828ff23b924a3610c9
Nicolas Phister <nicolas@phister.fr>
2019-02-26 14:18:10 +0100

    made AdjustableComponent not serializable

--- common/components/AdjustableComponent.hpp
+++ common/components/AdjustableComponent.hpp
@@ -11,7 +11,7 @@
 #include "reflection/Reflectible.hpp"
 
 namespace kengine {
-	class AdjustableComponent {
+	class AdjustableComponent : kengine::not_serializable {
 	public:
 		using string = putils::string<KENGINE_ADJUSTABLE_NAME_MAX_LENGTH>;
 
