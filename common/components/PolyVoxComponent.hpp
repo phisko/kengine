0565ffbbc384f341da7dbf77b4900eb4bd80a943
Nicolas Phister <nicolas@phister.fr>
2019-04-10 17:11:51 +0200

    added missing components

---
+++ common/components/PolyVoxComponent.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <PolyVox/RawVolume.h>
+
+struct PolyVoxModelComponent : kengine::not_serializable {
+	// Coupled with MeshComponent, indicates that this entity's mesh should be processed by PolyVoxShader
+	pmeta_get_class_name(PolyVoxModelComponent);
+};
+
+struct PolyVoxComponent : kengine::not_serializable {
+	struct VertexData {
+		float color[3] = { 0.f, 0.f, 0.f };
+
+		pmeta_get_attributes(
+			pmeta_reflectible_attribute(&VertexData::color)
+		);
+
+		bool operator==(size_t i) const {
+			return color[0] == 0.f && color[1] == 0.f && color[2] == 0.f;
+		}
+
+		bool operator>(size_t i) const {
+			return color[0] != 0.f || color[1] != 0.f || color[2] != 0.f;
+		}
+
+		bool operator==(const VertexData & rhs) const {
+			return color[0] == rhs.color[0] && color[1] == rhs.color[1] && color[2] == rhs.color[2];
+		}
+	};
+
+	static constexpr auto CHUNK_SIDE = 16;
+
+	PolyVox::RawVolume<VertexData> volume{ { { 0, 0, 0 }, { CHUNK_SIDE, CHUNK_SIDE, CHUNK_SIDE } } };
+	bool changed = true;
+
+	PolyVoxComponent() = default;
+
+	PolyVoxComponent(const PolyVoxComponent & rhs) noexcept {
+		*this = rhs;
+	}
+
+	PolyVoxComponent & operator=(const PolyVoxComponent & rhs) noexcept {
+		const auto & region = volume.getEnclosingRegion();
+
+		for (int z = region.getLowerZ(); z < region.getUpperZ(); ++z)
+			for (int y = region.getLowerY(); y < region.getUpperY(); ++y)
+				for (int x = region.getLowerX(); x < region.getUpperX(); ++x)
+					volume.setVoxel(x, y, z, rhs.volume.getVoxel(x, y, z));
+
+		return *this;
+	}
+
+	pmeta_get_class_name(PolyVoxComponent);
+};
+
---
+++ common/components/TexturedModelComponent.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+struct TexturedModelComponent : kengine::not_serializable {
+	// Used to indicate that this entity's ModelComponent is tied to a textured model
+	pmeta_get_class_name(TexturedModelComponent);
+};
