ca0ac5c29e1fbb8fc531e4869f28d0f0bcbf1675
Nicolas Phister] <nicolas@phister.fr>
2019-05-02 15:21:29 +0200

    cleanup

--- common/systems/assimp/AssImpShadowCube.cpp
+++ common/systems/assimp/AssImpShadowCube.cpp
@@ -69,35 +69,11 @@ namespace kengine {
 		putils::gl::setUniform(farPlane, radius);
 
 		for (const auto &[e, textured, modelComp, transform, skeleton] : _em.getEntities<TexturedModelComponent, kengine::ModelComponent, kengine::TransformComponent3f, SkeletonComponent>()) {
-			const auto & modelInfoEntity = _em.getEntity(modelComp.modelInfo);
-			if (!modelInfoEntity.has<ModelInfoComponent>())
-				continue;
-			const auto & modelInfo = modelInfoEntity.get<ModelInfoComponent>();
+			AssImpHelper::Locations locations;
+			locations.model = this->model;
+			locations.bones = this->bones;
 
-			putils::gl::setUniform(this->model, ShaderHelper::getModelMatrix(modelInfo, transform));
-
-			if (skeleton.meshes.empty()) {
-				static putils::vector<glm::mat4, KENGINE_SKELETON_MAX_BONES> defaultMats;
-				static bool first = true;
-				if (first) {
-					for (unsigned int i = 0; i < KENGINE_SKELETON_MAX_BONES; ++i)
-						defaultMats.push_back(glm::mat4(1.f));
-					first = false;
-				}
-				glUniformMatrix4fv(bones, KENGINE_SKELETON_MAX_BONES, GL_FALSE, glm::value_ptr(*defaultMats.begin()));
-			}
-
-			for (unsigned int i = 0; i < modelInfo.meshes.size(); ++i) {
-				const auto & meshInfo = modelInfo.meshes[i];
-
-				if (!skeleton.meshes.empty())
-					glUniformMatrix4fv(bones, KENGINE_SKELETON_MAX_BONES, GL_FALSE, glm::value_ptr(*skeleton.meshes[i].boneMats.begin()));
-
-				glBindVertexArray(meshInfo.vertexArrayObject);
-				glBindBuffer(GL_ARRAY_BUFFER, meshInfo.vertexBuffer);
-				glDrawElements(GL_TRIANGLES, meshInfo.nbIndices, meshInfo.indexType, nullptr);
-			}
-
+			AssImpHelper::drawModel(_em, modelComp, transform, skeleton, false, locations);
 		}
 
 		glViewport(0, 0, screenWidth, screenHeight);
