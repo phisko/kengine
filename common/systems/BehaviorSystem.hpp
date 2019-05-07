d47d9d23900d1f6451c4c544bed61d769592125a
Nicolas Phister] <nicolas@phister.fr>
2019-05-07 09:43:41 +0200

    made BehaviorSystem work with new EntityManager API

--- common/systems/BehaviorSystem.hpp
+++ common/systems/BehaviorSystem.hpp
@@ -1,18 +1,19 @@
 #pragma once
 
 #include "System.hpp"
+#include "EntityManager.hpp"
 #include "components/BehaviorComponent.hpp"
 
 namespace kengine {
 	class BehaviorSystem : public kengine::System<BehaviorSystem>
 	{
 	public:
-		BehaviorSystem(kengine::EntityManager & em) : _em(em) {}
+		BehaviorSystem(kengine::EntityManager & em) : System(em), _em(em) {}
 
 	public:
 		void execute() final {
-			for (const auto go : _em.getGameObjects<kengine::BehaviorComponent>())
-				go->getComponent<kengine::BehaviorComponent>().func();
+			for (const auto & [e, comp] : _em.getEntities<kengine::BehaviorComponent>())
+				comp.func();
 		}
 
 	private:
