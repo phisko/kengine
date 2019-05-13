327485c1a2c1785820ccae8ab28f510537fa6b16
Nicolas Phister] <nicolas@phister.fr>
2019-05-13 14:43:00 +0200

    added reflection to AnimationComponent family

--- common/components/AnimationComponent.hpp
+++ common/components/AnimationComponent.hpp
@@ -1,15 +1,13 @@
 #pragma once
 
-#ifndef KENGINE_ANIMATION_NAME_LENGTH
-# define KENGINE_ANIMATION_NAME_LENGTH 64
-#endif
-
 #ifndef KENGINE_ANIMATION_FILE_PATH_LENGTH
 # define KENGINE_ANIMATION_FILE_PATH_LENGTH 128
 #endif
 
 #include <vector>
+#define PUTILS_STRING_LENGTH KENGINE_ANIMATION_FILE_PATH_LENGTH
 #include "string.hpp"
+#undef PUTILS_STRING_LENGTH
 
 namespace kengine {
 	struct AnimationComponent : kengine::not_serializable {
@@ -18,21 +16,39 @@ namespace kengine {
 		float speed = 1.f;
 
 		pmeta_get_class_name(AnimationComponent);
+		pmeta_get_attributes(
+			pmeta_reflectible_attribute(&AnimationComponent::currentAnim),
+			pmeta_reflectible_attribute(&AnimationComponent::currentTime),
+			pmeta_reflectible_attribute(&AnimationComponent::speed)
+		);
 	};
 
 	struct AnimListComponent : kengine::not_serializable {
 		struct Anim {
-			putils::string<KENGINE_ANIMATION_NAME_LENGTH> name;
+			putils::string<KENGINE_ANIMATION_FILE_PATH_LENGTH> name;
 			float totalTime;
 			float ticksPerSecond;
+
+			pmeta_get_class_name(AnimListComponentAnim);
+			pmeta_get_attributes(
+				pmeta_reflectible_attribute(&Anim::name),
+				pmeta_reflectible_attribute(&Anim::totalTime),
+				pmeta_reflectible_attribute(&Anim::ticksPerSecond)
+			);
 		};
 		std::vector<Anim> allAnims;
 
 		pmeta_get_class_name(AnimListComponent);
+		pmeta_get_attributes(
+			pmeta_reflectible_attribute(&AnimListComponent::allAnims)
+		);
 	};
 
 	struct AnimFilesComponent {
 		std::vector<putils::string<KENGINE_ANIMATION_FILE_PATH_LENGTH>> files;
 		pmeta_get_class_name(AnimFilesComponent);
+		pmeta_get_attributes(
+			pmeta_reflectible_attribute(&AnimFilesComponent::files)
+		);
 	};
 }
