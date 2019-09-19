f50db25379225097cb385510bd983caaaf902215
Nicolas Phister] <nicolas@phister.fr>
2019-09-19 09:26:07 +0200

    made component editor display in columns like the adjustables

--- common/helpers/RegisterComponentEditor.hpp
+++ common/helpers/RegisterComponentEditor.hpp
@@ -25,14 +25,21 @@ namespace kengine {
 			inline kengine::EntityManager * g_em = nullptr;
 		}
 
+		template<typename F>
+		static void displayInColumns(const char * name, F && f) {
+			ImGui::Columns(2);
+			ImGui::Text(name);
+			ImGui::NextColumn();
+			f();
+			ImGui::Columns();
+		}
+
 		template<typename Member>
 		static void displayAttribute(const char * name, const Member & member) {
 			if constexpr (imguiEditor::has_member_c_str<Member>::value) {
-				ImGui::Columns(2);
-				ImGui::Text(name);
-				ImGui::NextColumn();
-				ImGui::Text(member.c_str());
-				ImGui::Columns();
+				displayInColumns(name, [&] {
+					ImGui::Text(member.c_str());
+				});
 			}
 			// else if constexpr (std::is_same_v<Member, const char *>::value)
 			// 	ImGui::LabelText(name, member);
@@ -47,17 +54,17 @@ namespace kengine {
 			}
 
 			else if constexpr (std::is_same_v<Member, putils::Color>) {
-				const auto normalized = putils::toNormalizedColor(member);
-				const ImVec4 col = { normalized.r, normalized.g, normalized.b, normalized.a };
-				ImGui::ColorButton(putils::string<64>(name) + "#" + (intptr_t)&member, col);
-				ImGui::SameLine();
-				ImGui::Text(name);
+				displayInColumns(name, [&] {
+					const auto normalized = putils::toNormalizedColor(member);
+					const ImVec4 col = { normalized.r, normalized.g, normalized.b, normalized.a };
+					ImGui::ColorButton(putils::string<64>(name) + "#" + (intptr_t)& member, col);
+				});
 			}
 			else if constexpr (std::is_same_v<Member, putils::NormalizedColor>) {
-				const ImVec4 col = { member.r, member.g, member.b, member.a };
-				ImGui::ColorButton(putils::string<64>(name) + "#" + (intptr_t)&member, col);
-				ImGui::SameLine();
-				ImGui::Text(name);
+				displayInColumns(name, [&] {
+					const ImVec4 col = { member.r, member.g, member.b, member.a };
+					ImGui::ColorButton(putils::string<64>(name) + "#" + (intptr_t)& member, col);
+				});
 			}
 
 			else if constexpr (putils::has_member_get_attributes<Member>::value) {
@@ -69,28 +76,26 @@ namespace kengine {
 				}
 			}
 			else {
-				ImGui::Columns(2);
-				ImGui::Text(name);
-				ImGui::NextColumn();
-				if constexpr (std::is_same_v<Member, kengine::Entity::ID>) {
-					if (ImGui::Button("Select"))
-						imguiEditor::g_em->getEntity(member).attach<kengine::SelectedComponent>();
-					ImGui::SameLine();
-					ImGui::Text("%zu", member);
-				}
-				else if constexpr (std::is_enum_v<Member>)
-					ImGui::Text(putils::magic_enum::enum_name(member).data());
-				else if constexpr (std::is_same_v<Member, bool>)
-					ImGui::Text(member ? "true" : "false");
-				else if constexpr (std::is_same_v<Member, int>)
-					ImGui::Text("%d", member);
-				else if constexpr (std::is_same_v<Member, unsigned int>)
-					ImGui::Text("%zu", member);
-				else if constexpr (std::is_same_v<Member, float>)
-					ImGui::Text("%f", member);
-				else if constexpr (std::is_same_v<Member, double>)
-					ImGui::Text("%d", member);
-				ImGui::Columns();
+				displayInColumns(name, [&] {
+					if constexpr (std::is_same_v<Member, kengine::Entity::ID>) {
+						if (ImGui::Button("Select"))
+							imguiEditor::g_em->getEntity(member).attach<kengine::SelectedComponent>();
+						ImGui::SameLine();
+						ImGui::Text("%zu", member);
+					}
+					else if constexpr (std::is_enum_v<Member>)
+						ImGui::Text(putils::magic_enum::enum_name(member).data());
+					else if constexpr (std::is_same_v<Member, bool>)
+						ImGui::Text(member ? "true" : "false");
+					else if constexpr (std::is_same_v<Member, int>)
+						ImGui::Text("%d", member);
+					else if constexpr (std::is_same_v<Member, unsigned int>)
+						ImGui::Text("%zu", member);
+					else if constexpr (std::is_same_v<Member, float>)
+						ImGui::Text("%f", member);
+					else if constexpr (std::is_same_v<Member, double>)
+						ImGui::Text("%d", member);
+				});
 			}
 		}
 
@@ -104,14 +109,23 @@ namespace kengine {
 			}
 		}
 
+		template<typename Member>
+		putils::string<64> getID(const char * name, Member && member) {
+			return putils::string<64>("##%s", name) + (intptr_t)&member;
+		}
+
 		template<typename MemberRef>
 		static void editAttribute(const char * name, MemberRef && member) {
 			using Member = std::decay_t<MemberRef>;
 
 			if constexpr (imguiEditor::has_member_c_str<Member>::value) {
-				putils::string<1024> s = member.c_str();
-				if (ImGui::InputText(name, s.begin(), s.max_size))
-					member = s.c_str();
+				displayInColumns(name, [&] {
+					putils::string<1024> s = member.c_str();
+					ImGui::PushItemWidth(-1.f);
+					if (ImGui::InputText(getID(name, member), s.begin(), s.max_size))
+						member = s.c_str();
+					ImGui::PopItemWidth();
+				});
 			}
 			// else if constexpr (std::is_same_v<Member, const char *>::value)
 			// 	ImGui::LabelText(name, member);
@@ -129,30 +143,31 @@ namespace kengine {
 			}
 
 			else if constexpr (std::is_same_v<Member, putils::Color>) {
-				auto normalized = putils::toNormalizedColor(member);
-				const ImVec4 col = { normalized.r, normalized.g, normalized.b, normalized.a };
+				displayInColumns(name, [&] {
+					auto normalized = putils::toNormalizedColor(member);
+					const ImVec4 col = { normalized.r, normalized.g, normalized.b, normalized.a };
 
-				if (ImGui::ColorButton(putils::string<64>(name) + "#" + (intptr_t)&member, col))
-					ImGui::OpenPopup("color picker popup");
+					if (ImGui::ColorButton(getID(name, member), col))
+						ImGui::OpenPopup(name);
 
-				if (ImGui::BeginPopup("#color picker popup")) {
-					if (ImGui::ColorPicker4(name, normalized.attributes))
-						member = putils::toColor(normalized);
-					ImGui::EndPopup();
-				}
-				ImGui::Text(name);
+					if (ImGui::BeginPopup(name)) {
+						if (ImGui::ColorPicker4(name, normalized.attributes))
+							member = putils::toColor(normalized);
+						ImGui::EndPopup();
+					}
+				});
 			}
 			else if constexpr (std::is_same_v<Member, putils::NormalizedColor>) {
-				const ImVec4 col = { member.r, member.g, member.b, member.a };
-				if (ImGui::ColorButton(putils::string<64>(name) + "#" + (intptr_t)&member, col))
-					ImGui::OpenPopup("color picker popup");
+				displayInColumns(name, [&] {
+					const ImVec4 col = { member.r, member.g, member.b, member.a };
+					if (ImGui::ColorButton(getID(name, member), col))
+						ImGui::OpenPopup(name);
 
-				if (ImGui::BeginPopup("color picker popup")) {
-					ImGui::ColorPicker4(name, member.attributes);
-					ImGui::EndPopup();
-				}
-				ImGui::SameLine();
-				ImGui::Text(name);
+					if (ImGui::BeginPopup(name)) {
+						ImGui::ColorPicker4(name, member.attributes);
+						ImGui::EndPopup();
+					}
+				});
 			}
 
 			else if constexpr (putils::has_member_get_attributes<Member>::value) {
@@ -165,33 +180,65 @@ namespace kengine {
 			}
 
 			else if constexpr (std::is_same_v<Member, kengine::Entity::ID>) {
-				if (ImGui::Button("Select"))
-					imguiEditor::g_em->getEntity(member).attach<kengine::SelectedComponent>();
-				ImGui::SameLine();
-				ImGui::InputScalar(name, sizeof(Member) == 64 ? ImGuiDataType_U64 : ImGuiDataType_U32, &member);
+				displayInColumns(name, [&] {
+					if (ImGui::Button("Select"))
+						imguiEditor::g_em->getEntity(member).attach<kengine::SelectedComponent>();
+					ImGui::SameLine();
+					ImGui::PushItemWidth(-1.f);
+					ImGui::InputScalar(getID(name, member), sizeof(Member) == 64 ? ImGuiDataType_U64 : ImGuiDataType_U32, &member);
+					ImGui::PopItemWidth();
+				});
 			}
 			else if constexpr (std::is_enum_v<Member>) {
-				static putils::string<64> names[putils::magic_enum::enum_count<Member>()];
-				static bool first = true;
-				if (first) {
-					for (int i = 0; i < lengthof(names); ++i)
-						names[i] = putils::magic_enum::enum_names<Member>()[i];
-					first = false;
-				}
-				ImGui::Combo(name, (int*)&member, [](void *, int idx, const char ** out) { *out = names[idx].c_str(); return true; }, nullptr, lengthof(names));
+				displayInColumns(name, [&] {
+					static putils::string<64> names[putils::magic_enum::enum_count<Member>()];
+					static bool first = true;
+					if (first) {
+						for (int i = 0; i < lengthof(names); ++i)
+							names[i] = putils::magic_enum::enum_names<Member>()[i];
+						first = false;
+					}
+					ImGui::Combo(getID(name, member), (int *)& member, [](void *, int idx, const char ** out) { *out = names[idx].c_str(); return true; }, nullptr, lengthof(names));
+				});
 			}
-			else if constexpr (std::is_same_v<Member, bool>)
-				ImGui::Checkbox(name, &member);
-			else if constexpr (std::is_same_v<Member, int>)
-				ImGui::InputInt(name, &member);
-			else if constexpr (std::is_same_v<Member, unsigned int>)
-				ImGui::InputScalar(name, sizeof(Member) == 64 ? ImGuiDataType_U64 : ImGuiDataType_U32, &member);
-			else if constexpr (std::is_same_v<Member, float>)
-				ImGui::InputFloat(name, &member);
-			else if constexpr (std::is_same_v<Member, double>)
-				ImGui::InputDouble(name, &member);
-			else
-				ImGui::Text(name);
+			else if constexpr (std::is_same_v<Member, bool>) {
+				displayInColumns(name, [&] {
+					ImGui::Checkbox(getID(name, member), &member);
+				});
+			}
+			else if constexpr (std::is_same_v<Member, int>) {
+				displayInColumns(name, [&] {
+					ImGui::PushItemWidth(-1.f);
+					ImGui::InputInt(getID(name, member), &member);
+					ImGui::PopItemWidth();
+				});
+			}
+			else if constexpr (std::is_same_v<Member, unsigned int>) {
+				displayInColumns(name, [&] {
+					ImGui::PushItemWidth(-1.f);
+					ImGui::InputScalar(getID(name, member), sizeof(Member) == 64 ? ImGuiDataType_U64 : ImGuiDataType_U32, &member);
+					ImGui::PopItemWidth();
+				});
+			}
+			else if constexpr (std::is_same_v<Member, float>) {
+				displayInColumns(name, [&] {
+					ImGui::PushItemWidth(-1.f);
+					ImGui::InputFloat(getID(name, member), &member);
+					ImGui::PopItemWidth();
+				});
+			}
+			else if constexpr (std::is_same_v<Member, double>) {
+				displayInColumns(name, [&] {
+					ImGui::PushItemWidth(-1.f);
+					ImGui::InputDouble(getID(name, member), &member);
+					ImGui::PopItemWidth();
+				});
+			}
+			else {
+				displayInColumns(name, [&] {
+					ImGui::Text("Unknown type");
+				});
+			}
 		}
 
 		template<typename Comp>
