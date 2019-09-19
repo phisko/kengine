e96e4c0f2634efad69d0460818850413cd63b841
Nicolas Phister] <nicolas@phister.fr>
2019-09-19 09:25:39 +0200

    improved entity editor layout

--- common/systems/ImGuiEntityEditorSystem.cpp
+++ common/systems/ImGuiEntityEditorSystem.cpp
@@ -21,44 +21,56 @@ auto ImGuiEntityEditor(kengine::EntityManager & em) {
 				return;
 
 			for (auto &[selected, _] : em.getEntities<kengine::SelectedComponent>()) {
-				bool open = true;
-				if (ImGui::Begin(putils::string<64>("[%d] Entity editor", selected.id), &open)) {
+				ImGui::SetNextWindowSize({ 200.f, 200.f}, ImGuiCond_FirstUseEver);
+
+				if (ImGui::Begin(putils::string<64>("[%d] Entity editor", selected.id), nullptr, ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoTitleBar)) {
+
+					if (ImGui::Button("x"))
+						selected.detach<kengine::SelectedComponent>();
+					ImGui::SameLine();
+					ImGui::Text("Search");
+					ImGui::SameLine();
 					static char nameSearch[1024] = "";
-					ImGui::InputText("Component", nameSearch, sizeof(nameSearch));
+					ImGui::PushItemWidth(-1.f);
+					ImGui::InputText("##Search", nameSearch, sizeof(nameSearch));
+					ImGui::PopItemWidth();
 
-					const auto components = em.getComponentFunctionMaps();
-					if (ImGui::CollapsingHeader("Edit"))
-						for (const auto comp : components) {
-							const auto has = comp->getFunction<kengine::functions::Has>();
-							const auto edit = comp->getFunction<kengine::functions::EditImGui>();
-							if (has != nullptr && edit != nullptr && strstr(comp->name, nameSearch) && has(selected))
-								if (ImGui::TreeNode(comp->name)) {
-									edit(selected);
-									ImGui::TreePop();
-								}
-						}
+					{
+						ImGui::BeginChild("##child");
+						const auto components = em.getComponentFunctionMaps();
+						if (ImGui::CollapsingHeader("Edit"))
+							for (const auto comp : components) {
+								const auto has = comp->getFunction<kengine::functions::Has>();
+								const auto edit = comp->getFunction<kengine::functions::EditImGui>();
+								if (has != nullptr && edit != nullptr && strstr(comp->name, nameSearch) && has(selected))
+									if (ImGui::TreeNode(putils::string<64>(comp->name) + "##edit")) {
+										edit(selected);
+										ImGui::TreePop();
+									}
+							}
 
-					if (ImGui::CollapsingHeader("Add"))
-						for (const auto comp : components) {
-							const auto has = comp->getFunction<kengine::functions::Has>();
-							const auto add = comp->getFunction<kengine::functions::Attach>();
-							if (has != nullptr && add != nullptr && strstr(comp->name, nameSearch) && !has(selected))
-								if (ImGui::Button(comp->name))
-									add(selected);
-						}
+						if (ImGui::CollapsingHeader("Add"))
+							for (const auto comp : components) {
+								const auto has = comp->getFunction<kengine::functions::Has>();
+								const auto add = comp->getFunction<kengine::functions::Attach>();
+								if (has != nullptr && add != nullptr && strstr(comp->name, nameSearch) && !has(selected))
+									if (ImGui::Button(putils::string<64>(comp->name) + "##add"))
+										add(selected);
+							}
 
-					if (ImGui::CollapsingHeader("Remove"))
-						for (const auto comp : components) {
-							const auto has = comp->getFunction<kengine::functions::Has>();
-							const auto remove = comp->getFunction<kengine::functions::Detach>();
-							if (has != nullptr && remove != nullptr && strstr(comp->name, nameSearch) && has(selected))
-								if (ImGui::Button(putils::string<64>(comp->name) + "##remove"))
-									remove(selected);
-						}
+						if (ImGui::CollapsingHeader("Remove"))
+							for (const auto comp : components) {
+								const auto has = comp->getFunction<kengine::functions::Has>();
+								const auto remove = comp->getFunction<kengine::functions::Detach>();
+								if (has != nullptr && remove != nullptr && strstr(comp->name, nameSearch) && has(selected))
+									if (ImGui::Button(putils::string<64>(comp->name) + "##remove"))
+										remove(selected);
+							}
+
+						ImGui::EndChild();
+					}
 				}
 				ImGui::End();
-				if (!open)
-					selected.detach<kengine::SelectedComponent>();
 			}
 		});
 	};
