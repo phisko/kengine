981848b1b55a94873ff4619e7fb753a380130437
Nicolas Phister] <nicolas@phister.fr>
2019-10-17 14:35:59 +0200

    ogre -- made ImGuiManager not call NewFrame if there is no camera

--- common/systems/ogre/ImGuiManager.cpp
+++ common/systems/ogre/ImGuiManager.cpp
@@ -6,6 +6,7 @@
 #include "ImGuiOverlay.h"
 #include "ImGuiInputListener.h"
 #include "components/ImGuiComponent.hpp"
+#include "components/CameraComponent.hpp"
 
 ImGuiManager::ImGuiManager(kengine::EntityManager & em, OgreBites::ApplicationContext & app) : _em(em) {
 	auto imguiOverlay = new Ogre::kengine::ImGuiOverlay;
@@ -15,11 +16,26 @@ ImGuiManager::ImGuiManager(kengine::EntityManager & em, OgreBites::ApplicationCo
 	app.addInputListener(new OgreBites::kengine::ImGuiInputListener);
 }
 
+static size_t g_nbCameras = 0;
+
 bool ImGuiManager::frameStarted(const Ogre::FrameEvent & e) noexcept {
+	if (g_nbCameras == 0)
+		return true;
+
 	Ogre::kengine::ImGuiOverlay::NewFrame(e);
 
 	for (const auto & [e, comp] : _em.getEntities<kengine::ImGuiComponent>())
 		comp.display(GImGui);
 
 	return true;
+}
+
+void ImGuiManager::registerEntity(kengine::Entity & e) noexcept {
+	if (e.has<kengine::CameraComponent3f>())
+		++g_nbCameras;
+}
+
+void ImGuiManager::removeEntity(kengine::Entity & e) noexcept {
+	if (e.has<kengine::CameraComponent3f>())
+		--g_nbCameras;
 }
--- common/systems/ogre/ImGuiManager.hpp
+++ common/systems/ogre/ImGuiManager.hpp
@@ -7,7 +7,9 @@ class ImGuiManager : public Manager {
 public:
 	ImGuiManager(kengine::EntityManager & em, OgreBites::ApplicationContext & app);
 
-	virtual bool frameStarted(const Ogre::FrameEvent & e) noexcept;
+	void registerEntity(kengine::Entity & e) noexcept override;
+	void removeEntity(kengine::Entity & e) noexcept override;
+	bool frameStarted(const Ogre::FrameEvent & e) noexcept override;
 
 private:
 	kengine::EntityManager & _em;
