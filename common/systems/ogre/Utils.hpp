541314ef5bc60c518bf0453ca650b637ab157e30
Nicolas Phister] <nicolas@phister.fr>
2019-10-18 17:26:56 +0200

    ogre -- add convert(color)

--- common/systems/ogre/Utils.hpp
+++ common/systems/ogre/Utils.hpp
@@ -5,3 +5,6 @@
 
 static Ogre::Vector3 convert(const putils::Point3f & p) { return { p.x, p.y, p.z }; }
 static putils::Point3f convert(const Ogre::Vector3 & p) { return { p.x, p.y, p.z }; }
+
+static Ogre::ColourValue convert(const putils::NormalizedColor & color) { return Ogre::ColourValue{ color.r, color.g, color.b, color.a }; }
+static putils::NormalizedColor convert(const Ogre::ColourValue & color) { return { color.r, color.g, color.b, color.a }; }
