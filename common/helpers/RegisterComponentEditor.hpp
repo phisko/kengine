abe7253f363d0d9343159bff6d53822660142e55
Nicolas Phister] <nicolas@phister.fr>
2019-11-19 14:31:32 +0100

    use putils::reflection for component editors and json loaders

--- common/helpers/RegisterComponentEditor.hpp
+++ common/helpers/RegisterComponentEditor.hpp
@@ -1,13 +1,8 @@
 #pragma once
 
 #include "EntityManager.hpp"
-#include "Color.hpp"
-#include "components/SelectedComponent.hpp"
-#include "reflection.hpp"
 #include "functions/ImGuiEditor.hpp"
-#include "imgui.h"
-#include "magic_enum.hpp"
-#include "lengthof.hpp"
+#include "reflection/imgui_helper.hpp"
 
 namespace kengine {
 	template<typename Comp>
@@ -19,254 +14,21 @@ namespace kengine {
 
 namespace kengine {
 	namespace detail {
-		namespace imguiEditor {
-			putils_member_detector(c_str);
-			putils_member_detector(emplace_back);
-			inline kengine::EntityManager * g_em = nullptr;
-		}
-
-		template<typename F>
-		static void displayInColumns(const char * name, F && f) {
-			ImGui::Columns(2);
-			ImGui::Text(name);
-			ImGui::NextColumn();
-			f();
-			ImGui::Columns();
-		}
-
-		template<typename Member>
-		static void displayAttribute(const char * name, const Member & member) {
-			if constexpr (imguiEditor::has_member_c_str<Member>::value) {
-				displayInColumns(name, [&] {
-					ImGui::Text(member.c_str());
-				});
-			}
-			// else if constexpr (std::is_same_v<Member, const char *>::value)
-			// 	ImGui::LabelText(name, member);
-
-			else if constexpr (putils::is_iterable<Member>::value) {
-				if (ImGui::TreeNode(name)) {
-					int i = 0;
-					for (const auto & val : member)
-						displayAttribute(putils::string<64>("%d", i++), val);
-					ImGui::TreePop();
-				}
-			}
-
-			else if constexpr (std::is_same_v<Member, putils::Color>) {
-				displayInColumns(name, [&] {
-					const auto normalized = putils::toNormalizedColor(member);
-					const ImVec4 col = { normalized.r, normalized.g, normalized.b, normalized.a };
-					ImGui::ColorButton(putils::string<64>(name) + "#" + (intptr_t)& member, col);
-				});
-			}
-			else if constexpr (std::is_same_v<Member, putils::NormalizedColor>) {
-				displayInColumns(name, [&] {
-					const ImVec4 col = { member.r, member.g, member.b, member.a };
-					ImGui::ColorButton(putils::string<64>(name) + "#" + (intptr_t)& member, col);
-				});
-			}
-
-			else if constexpr (putils::reflection::has_attributes<Member>::value) {
-				if (ImGui::TreeNode(name)) {
-					putils::reflection::for_each_attribute<Member>([&member](const char * name, const auto attr) {
-						displayAttribute(name, member.*attr);
-					});
-					ImGui::TreePop();
-				}
-			}
-			else {
-				displayInColumns(name, [&] {
-					if constexpr (std::is_same_v<Member, kengine::Entity::ID>) {
-						if (ImGui::Button("Select"))
-							imguiEditor::g_em->getEntity(member).attach<kengine::SelectedComponent>();
-						ImGui::SameLine();
-						ImGui::Text("%zu", member);
-					}
-					else if constexpr (std::is_enum_v<Member>)
-						ImGui::Text(putils::magic_enum::enum_name(member).data());
-					else if constexpr (std::is_same_v<Member, bool>)
-						ImGui::Text(member ? "true" : "false");
-					else if constexpr (std::is_same_v<Member, int>)
-						ImGui::Text("%d", member);
-					else if constexpr (std::is_same_v<Member, unsigned int>)
-						ImGui::Text("%zu", member);
-					else if constexpr (std::is_same_v<Member, float>)
-						ImGui::Text("%f", member);
-					else if constexpr (std::is_same_v<Member, double>)
-						ImGui::Text("%d", member);
-				});
-			}
-		}
-
 		template<typename Comp>
 		static void displayComponent(const kengine::Entity & e) {
-			if constexpr (putils::reflection::has_attributes<Comp>::value) {
-				const auto & comp = e.get<Comp>();
-				putils::reflection::for_each_attribute<Comp>([&comp](const char * name, const auto member) {
-					displayAttribute(name, comp.*member);
-				});
-			}
-		}
-
-		template<typename Member>
-		putils::string<64> getID(const char * name, Member && member) {
-			return putils::string<64>("##%s", name) + (intptr_t)&member;
-		}
-
-		template<typename MemberRef>
-		static void editAttribute(const char * name, MemberRef && member) {
-			using Member = std::remove_reference_t<MemberRef>;
-
-			if constexpr (imguiEditor::has_member_c_str<Member>::value) {
-				displayInColumns(name, [&] {
-					putils::string<1024> s = member.c_str();
-					ImGui::PushItemWidth(-1.f);
-					if (ImGui::InputText(getID(name, member), s.begin(), s.max_size, ImGuiInputTextFlags_EnterReturnsTrue))
-						member = s.c_str();
-					ImGui::PopItemWidth();
-				});
-			}
-			// else if constexpr (std::is_same_v<Member, const char *>::value)
-			// 	ImGui::LabelText(name, member);
-
-			else if constexpr (putils::is_iterable<Member>::value) {
-				if (ImGui::TreeNode(name)) {
-					if constexpr (imguiEditor::has_member_emplace_back<Member>::value)
-						if (ImGui::Button("Add"))
-							member.emplace_back();
-					int i = 0;
-					for (auto & val : member)
-						editAttribute(putils::string<64>("%d", i++), val);
-					ImGui::TreePop();
-				}
-			}
-
-			else if constexpr (std::is_same_v<Member, putils::Color>) {
-				displayInColumns(name, [&] {
-					auto normalized = putils::toNormalizedColor(member);
-					const ImVec4 col = { normalized.r, normalized.g, normalized.b, normalized.a };
-
-					if (ImGui::ColorButton(getID(name, member), col))
-						ImGui::OpenPopup(name);
-
-					if (ImGui::BeginPopup(name)) {
-						if (ImGui::ColorPicker4(name, normalized.attributes))
-							member = putils::toColor(normalized);
-						ImGui::EndPopup();
-					}
-				});
-			}
-			else if constexpr (std::is_same_v<Member, putils::NormalizedColor>) {
-				displayInColumns(name, [&] {
-					const ImVec4 col = { member.r, member.g, member.b, member.a };
-					if (ImGui::ColorButton(getID(name, member), col))
-						ImGui::OpenPopup(name);
-
-					if (ImGui::BeginPopup(name)) {
-						ImGui::ColorPicker4(name, member.attributes);
-						ImGui::EndPopup();
-					}
-				});
-			}
-
-			else if constexpr (putils::reflection::has_attributes<Member>::value) {
-				if (ImGui::TreeNode(name)) {
-					putils::reflection::for_each_attribute<Member>([&member](const char * name, const auto attr) {
-						editAttribute(name, member.*attr);
-					});
-					ImGui::TreePop();
-				}
-			}
-
-			else if constexpr (std::is_same_v<Member, kengine::Entity::ID>) {
-				displayInColumns(name, [&] {
-					if (ImGui::Button("Select"))
-						imguiEditor::g_em->getEntity(member).attach<kengine::SelectedComponent>();
-					ImGui::SameLine();
-					ImGui::PushItemWidth(-1.f);
-					auto val = member;
-					if (ImGui::InputScalar(getID(name, member), sizeof(Member) == 64 ? ImGuiDataType_U64 : ImGuiDataType_U32, &val, nullptr, nullptr, nullptr, ImGuiInputTextFlags_EnterReturnsTrue))
-						member = val;
-					ImGui::PopItemWidth();
-				});
-			}
-			else if constexpr (std::is_enum_v<Member>) {
-				static_assert(std::is_same_v<std::underlying_type_t<Member>, int>);
-
-				displayInColumns(name, [&] {
-					static putils::string<64> names[putils::magic_enum::enum_count<Member>()];
-					static bool first = true;
-					if (first) {
-						for (int i = 0; i < lengthof(names); ++i)
-							names[i] = putils::magic_enum::enum_names<Member>()[i];
-						first = false;
-					}
-					ImGui::Combo(getID(name, member), (int *)& member, [](void *, int idx, const char ** out) { *out = names[idx].c_str(); return true; }, nullptr, lengthof(names));
-				});
-			}
-			else if constexpr (std::is_same_v<Member, bool>) {
-				displayInColumns(name, [&] {
-					ImGui::Checkbox(getID(name, member), &member);
-				});
-			}
-			else if constexpr (std::is_same_v<Member, int>) {
-				displayInColumns(name, [&] {
-					ImGui::PushItemWidth(-1.f);
-					auto val = member;
-					if (ImGui::InputInt(getID(name, member), &val, ImGuiInputTextFlags_EnterReturnsTrue))
-						member = val;
-					ImGui::PopItemWidth();
-				});
-			}
-			else if constexpr (std::is_same_v<Member, unsigned int>) {
-				displayInColumns(name, [&] {
-					ImGui::PushItemWidth(-1.f);
-					auto val = member;
-					if (ImGui::InputScalar(getID(name, member), sizeof(Member) == 64 ? ImGuiDataType_U64 : ImGuiDataType_U32, &val, nullptr, nullptr, nullptr, ImGuiInputTextFlags_EnterReturnsTrue))
-						member = val;
-					ImGui::PopItemWidth();
-				});
-			}
-			else if constexpr (std::is_same_v<Member, float>) {
-				displayInColumns(name, [&] {
-					ImGui::PushItemWidth(-1.f);
-					auto val = member;
-					if (ImGui::InputFloat(getID(name, member), &val, 0.f, 0.f, "%.6f", ImGuiInputTextFlags_EnterReturnsTrue))
-						member = val;
-					ImGui::PopItemWidth();
-				});
-			}
-			else if constexpr (std::is_same_v<Member, double>) {
-				displayInColumns(name, [&] {
-					ImGui::PushItemWidth(-1.f);
-					auto val = member;
-					if (ImGui::InputDouble(getID(name, member), &val, 0.0, 0.0, "%.6f", ImGuiInputTextFlags_EnterReturnsTrue))
-						member = val;
-					ImGui::PopItemWidth();
-				});
-			}
-			else {
-				displayInColumns(name, [&] {
-					ImGui::Text("Unknown type");
-				});
-			}
+			if constexpr (putils::reflection::has_attributes<Comp>::value)
+				putils::reflection::imguiDisplay(e.get<Comp>());
 		}
 
 		template<typename Comp>
 		static void editComponent(kengine::Entity & e) {
-			if constexpr (putils::reflection::has_attributes<Comp>::value) {
-				auto & comp = e.get<Comp>();
-				putils::reflection::for_each_attribute<Comp>([&comp](const char * name, const auto member) {
-					editAttribute(name, comp.*member);
-				});
-			}
+			if constexpr (putils::reflection::has_attributes<Comp>::value)
+				putils::reflection::imguiEdit(e.get<Comp>());
 		}
 	}
 
 	template<typename Comp>
 	void registerComponentEditor(kengine::EntityManager & em) {
-		detail::imguiEditor::g_em = &em;
 		em.registerComponentFunction<Comp>(functions::DisplayImGui{ detail::displayComponent<Comp> });
 		em.registerComponentFunction<Comp>(functions::EditImGui{ detail::editComponent<Comp> });
 	}
--- common/helpers/RegisterComponentJSONLoader.hpp
+++ common/helpers/RegisterComponentJSONLoader.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
 #include "EntityManager.hpp"
-#include "json.hpp"
-#include "magic_enum.hpp"
-
+#include "reflection/json_helper.hpp"
 #include "functions/LoadFromJSON.hpp"
 
 namespace kengine {
@@ -16,60 +14,13 @@ namespace kengine {
 
 namespace kengine {
 	namespace detail {
-		namespace componentJSONLoader {
-			putils_member_detector(c_str);
-		}
-		template<typename MemberRef>
-		static void loadJSONObject(const putils::json & object, MemberRef && member) {
-			using Member = std::remove_reference_t<MemberRef>;
-
-			if constexpr (componentJSONLoader::has_member_c_str<Member>::value)
-				member = object.get<std::string>().c_str();
-			else if constexpr (putils::is_std_vector<Member>::value) {
-				for (const auto & it : object) {
-					member.emplace_back();
-					auto & element = member.back();
-					loadJSONObject(it, element);
-				}
-			}
-			else if constexpr (putils::is_vector<Member>::value) {
-				for (const auto & it : object) {
-					auto & element = member.emplace_back();
-					loadJSONObject(it, element);
-				}
-			}
-			else if constexpr (std::is_array<Member>::value) {
-				size_t i = 0;
-				assert(object.array().size() <= lengthof(member));
-				for (const auto & it : object) {
-					auto & element = member[i];
-					loadJSONObject(it, element);
-					++i;
-				}
-			}
-			else if constexpr (putils::reflection::has_attributes<Member>::value) {
-				putils::reflection::for_each_attribute<Member>([&member, &object](const char * name, const auto attr) {
-					const auto attrJSON = object.find(name);
-					if (attrJSON != object.end())
-						loadJSONObject(*attrJSON, member.*attr);
-				});
-			}
-			else if constexpr (std::is_enum<Member>::value) {
-				for (const auto & p : putils::magic_enum::enum_entries<Member>())
-					if (object == putils::string<64>(p.second).c_str())
-						member = p.first;
-			}
-			else if constexpr (std::is_scalar<Member>::value)
-				member = object;
-		}
-
 		template<typename Component>
 		static void loadJSONComponent(const putils::json & jsonEntity, kengine::Entity & e) {
 			const auto it = jsonEntity.find(putils::reflection::get_class_name<Component>());
 			if (it == jsonEntity.end())
 				return;
 			auto & comp = e.attach<Component>();
-			loadJSONObject(*it, comp);
+			putils::reflection::fromJSON(*it, comp);
 		}
 	}
 
