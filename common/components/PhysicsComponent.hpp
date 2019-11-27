46f9aacf5f896b93760d37d5f89bd8bc83adfc1f
Nicolas Phister] <nicolas@phister.fr>
2019-11-27 16:59:00 +0100

    add pitch + yaw + roll to PhysicsComponent

--- common/components/PhysicsComponent.hpp
+++ common/components/PhysicsComponent.hpp
@@ -9,7 +9,10 @@ namespace kengine {
 		float mass = 1.f;
 
         putils::Vector3f movement;
-        float speed = 1;
+		float yaw = 0.f; // radians to turn in frame
+		float pitch = 0.f; // radians to turn in frame
+		float roll = 0.f; // radians to turn in frame
+        float speed = 1.f;
 		bool changed = false;
 
         putils_reflection_class_name(PhysicsComponent);
@@ -19,6 +22,9 @@ namespace kengine {
                 putils_reflection_attribute(&PhysicsComponent::mass),
 
                 putils_reflection_attribute(&PhysicsComponent::movement),
+                putils_reflection_attribute(&PhysicsComponent::yaw),
+                putils_reflection_attribute(&PhysicsComponent::pitch),
+                putils_reflection_attribute(&PhysicsComponent::roll),
                 putils_reflection_attribute(&PhysicsComponent::speed),
                 putils_reflection_attribute(&PhysicsComponent::changed)
         );
