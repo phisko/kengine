00f120589d243ae389b0d3129053781e92f503dd
Nicolas Phister] <nicolas@phister.fr>
2020-03-05 14:53:50 +0100

    fix segfault in glew memset

--- systems/opengl/libs/glew/src/glew.c
+++ systems/opengl/libs/glew/src/glew.c
@@ -33,7 +33,7 @@
 #pragma function(memset)
 void * __cdecl memset(void * ptr, int value, size_t num) {
 	for (size_t i = 0; i < num; ++i)
-		((int *)ptr)[i] = value;
+		((char *)ptr)[i] = value;
     return ptr;
 }
 
