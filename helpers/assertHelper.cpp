3f51b31b67185fed778c4a9ea4a4ce6bd7eb498e
Nicolas Phister <nicolas@phister.fr>
2021-01-13 14:18:42 +0100

    use putils::getCallStack

--- helpers/assertHelper.cpp
+++ helpers/assertHelper.cpp
@@ -8,8 +8,9 @@
 #include "imgui.h"
 #include "lengthof.hpp"
 #include "string.hpp"
+#include "get_call_stack.hpp"
 
-#ifdef WIN32
+#ifdef _WIN32
 # include "windows.h"
 # include "dbghelp.h"
 #endif
@@ -37,7 +38,7 @@ namespace kengine::assertHelper {
 					info.file = file;
 					info.line = line;
 					info.expr = expr;
-					info.stack = getCallStack();
+					info.stack = putils::getCallStack();
 
 					e += TimeModulatorComponent{ 0.f };
 
@@ -77,39 +78,6 @@ namespace kengine::assertHelper {
 					} };
 				};
 
-			}
-
-			static std::string getCallStack() noexcept {
-				std::string ret;
-#ifdef WIN32
-				const auto process = GetCurrentProcess();
-				SymInitialize(process, nullptr, true);
-
-				void * stack[128];
-				const auto frames = CaptureStackBackTrace(0, (DWORD)putils::lengthof(stack), stack, nullptr);
-
-				char symbolBuffer[sizeof(SYMBOL_INFO) + 256];
-				auto symbol = (SYMBOL_INFO *)symbolBuffer;
-				symbol->MaxNameLen = 256;
-				symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
-
-				for (int i = 0; i < frames; i++) {
-					SymFromAddr(process, (DWORD64)(stack[i]), 0, symbol);
-
-					DWORD  displacement;
-					IMAGEHLP_LINE64 line;
-					SymGetLineFromAddr64(process, (DWORD64)(stack[i]), &displacement, &line);
-
-					static constexpr auto stackFramesToIgnore = 5;
-					if (i >= stackFramesToIgnore) {
-						const putils::string<256> s("\t %i: %s - (l.%i)", frames - i - 1, symbol->Name, line.LineNumber);
-						if (!ret.empty())
-							ret += '\n';
-						ret += s;
-					}
-				}
-#endif
-				return ret;
 			}
 
 			static void ignore(const char * file, int line) noexcept {
