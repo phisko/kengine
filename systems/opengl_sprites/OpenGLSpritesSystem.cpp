18aba6b82b227f9c9612cda9ff1c56628398fa2a
Nicolas Phister <nicolas@phister.fr>
2021-01-20 11:22:52 +0100

    make OpenGLSpritesSystem use resourceHelper

--- systems/opengl_sprites/OpenGLSpritesSystem.cpp
+++ systems/opengl_sprites/OpenGLSpritesSystem.cpp
@@ -3,15 +3,11 @@
 #include "kengine.hpp"
 #include "SpritesShader.hpp"
 #include "data/InstanceComponent.hpp"
-#include "data/ModelComponent.hpp"
-#include "data/TextureDataComponent.hpp"
 #include "data/ShaderComponent.hpp"
 #include "data/GraphicsComponent.hpp"
-#include "data/SpriteComponent.hpp"
 
 #include "functions/OnEntityCreated.hpp"
-
-#include "stb_image.h"
+#include "helpers/resourceHelper.hpp"
 
 namespace kengine {
 	EntityCreator * OpenGLSpritesSystem() noexcept {
@@ -26,36 +22,11 @@ namespace kengine {
 				if (!e.has<GraphicsComponent>())
 					return;
 
-				if (!e.has<SpriteComponent2D>() && !e.has<SpriteComponent3D>())
-					return;
-
-				if (e.has<InstanceComponent>() && e.get<InstanceComponent>().model != INVALID_ID)
-					return;
-
 				const auto & file = e.get<GraphicsComponent>().appearance;
-
-				for (const auto & [model, comp] : entities.with<ModelComponent>())
-					if (comp.file == file) {
-						e += InstanceComponent{ model.id };
-						return;
-					}
-
-				int width, height, components;
-				const auto data = stbi_load(file.c_str(), &width, &height, &components, 0);
-				if (data == nullptr)
-					return; // Not supported image type
-
-				entities += [&](Entity & model) noexcept {
-					e += InstanceComponent{ model.id };
-
-					TextureDataComponent textureData;
-					textureData.data = data;
-					textureData.width = width;
-					textureData.height = height;
-					textureData.components = components;
-					textureData.free = stbi_image_free;
-					model += std::move(textureData);
-				};
+				const auto model = resourceHelper::loadTexture(file);
+				if (model == INVALID_ID)
+					return;
+				e += InstanceComponent{ model };
 			}
 		};
 
