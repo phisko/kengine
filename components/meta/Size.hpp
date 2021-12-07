0df928c601412a10b53157d91a18f7d9d71c59d2
Nicolas Phister <nicolas.phister@dont-nod.com>
2021-12-07 14:59:04 +0100

    make meta::Size reflectable

--- components/meta/Size.hpp
+++ components/meta/Size.hpp
@@ -1,7 +1,18 @@
 #pragma once
 
+#include "reflection.hpp"
+
 namespace kengine::meta {
     struct Size {
         size_t size;
     };
 }
+
+#define refltype kengine::meta::Size
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(size)
+    )
+};
+#undef refltype
