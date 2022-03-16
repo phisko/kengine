aaf1628e7fa3a15027ea9e06e9f0253c86a83a03
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-03-16 10:42:22 +0100

    reflectible CommandLineComponent

--- components/data/CommandLineComponent.hpp
+++ components/data/CommandLineComponent.hpp
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <vector>
+#include <string>
+#include "reflection.hpp"
+
 namespace kengine {
     struct CommandLineComponent {
-        int argc = 0;
-        const char ** argv = nullptr;
+        std::vector<std::string> arguments;
     };
 }
+
+#define refltype kengine::CommandLineComponent
+putils_reflection_info{
+    putils_reflection_class_name;
+    putils_reflection_attributes(
+        putils_reflection_attribute(arguments)
+    );
+};
+#undef refltype
