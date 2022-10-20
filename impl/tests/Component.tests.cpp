c3b4ab35451d7b5c485fddcdc637b2e8c24c19b8
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-10-20 17:00:06 +0200

    add Component::dtor test

--- impl/tests/Component.tests.cpp
+++ impl/tests/Component.tests.cpp
@@ -15,3 +15,36 @@ TEST_F(Component, id) {
     EXPECT_EQ(kengine::impl::Component<int>::id(), 0);
     EXPECT_EQ(kengine::impl::Component<float>::id(), 2);
 }
+
+TEST_F(Component, dtor) {
+    static int dtorCount = 0;
+    struct TestComponent {
+        std::optional<int> value;
+
+        ~TestComponent() noexcept {
+            if (value)
+                ++dtorCount;
+        }
+
+        TestComponent(int value) noexcept
+        : value(value)
+        {}
+
+        TestComponent() noexcept = default;
+        TestComponent(TestComponent &&) noexcept = default;
+        TestComponent & operator=(TestComponent &&) noexcept = default;
+    };
+
+    auto & v = kengine::impl::Component<TestComponent>::set(0);
+    v.value = 42;
+    kengine::impl::Component<TestComponent>::metadata().reset(0);
+    EXPECT_EQ(dtorCount, 1);
+
+    {
+        TestComponent source{ 42 };
+        kengine::impl::Component<TestComponent>::set(1, std::move(source));
+        EXPECT_EQ(dtorCount, 1);
+    }
+
+    EXPECT_EQ(dtorCount, 2);
+}
