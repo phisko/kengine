da6f98ce58855c3f7781b4e61980b8e5e6666c98
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-10-27 15:15:45 +0200

    add nearPlane and farPlane to CameraComponent

--- components/data/CameraComponent.hpp
+++ components/data/CameraComponent.hpp
@@ -7,7 +7,9 @@ namespace kengine {
         putils::Rect3f frustum{ { 0.f, 0.f, 0.f }, { 1.f, 1.f, 1.f } };
         float pitch = 0; // Radians
         float yaw = 0; // Radians
-		float roll = 0; // Radians
+        float roll = 0; // Radians
+        float nearPlane = 1.f;
+        float farPlane = 1000.f;
 	};
 };
 
@@ -17,8 +19,10 @@ putils_reflection_info{
 	putils_reflection_attributes(
 		putils_reflection_attribute(frustum),
 		putils_reflection_attribute(pitch),
-		putils_reflection_attribute(yaw),
-		putils_reflection_attribute(roll)
+        putils_reflection_attribute(yaw),
+        putils_reflection_attribute(roll),
+        putils_reflection_attribute(nearPlane),
+        putils_reflection_attribute(farPlane)
 	);
 };
 #undef refltype
