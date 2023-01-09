c872fb9c11ec798e95f91f1c0c28d2f59ec853ea
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-01-09 09:24:08 +0100

    make all components reflectible

--- kengine/data/input_buffer.hpp
+++ kengine/data/input_buffer.hpp
@@ -50,5 +50,72 @@ namespace kengine::data {
 #define refltype kengine::data::input_buffer
 putils_reflection_info {
 	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(keys),
+		putils_reflection_attribute(clicks),
+		putils_reflection_attribute(moves),
+		putils_reflection_attribute(scrolls)
+	);
+	putils_reflection_used_types(
+		putils_reflection_type(refltype::key_event),
+		putils_reflection_type(refltype::click_event),
+		putils_reflection_type(refltype::mouse_move_event),
+		putils_reflection_type(refltype::mouse_scroll_event)
+	);
+};
+#undef refltype
+
+#define refltype kengine::data::input_buffer::key_event
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(window),
+		putils_reflection_attribute(key),
+		putils_reflection_attribute(pressed)
+	);
+};
+#undef refltype
+
+#define refltype kengine::data::input_buffer::click_event
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(window),
+		putils_reflection_attribute(pos),
+		putils_reflection_attribute(button),
+		putils_reflection_attribute(pressed)
+	);
+	putils_reflection_used_types(
+		putils_reflection_type(putils::point2f)
+	);
+};
+#undef refltype
+
+#define refltype kengine::data::input_buffer::mouse_move_event
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(window),
+		putils_reflection_attribute(pos),
+		putils_reflection_attribute(rel)
+	);
+	putils_reflection_used_types(
+		putils_reflection_type(putils::point2f)
+	);
+};
+#undef refltype
+
+#define refltype kengine::data::input_buffer::mouse_scroll_event
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(window),
+		putils_reflection_attribute(xoffset),
+		putils_reflection_attribute(yoffset),
+		putils_reflection_attribute(pos)
+	);
+	putils_reflection_used_types(
+		putils_reflection_type(putils::point2f)
+	);
 };
 #undef refltype
--- kengine/functions/log.hpp
+++ kengine/functions/log.hpp
@@ -28,3 +28,35 @@ namespace kengine {
 		struct log : base_function<log_signature> {};
 	}
 }
+
+#define refltype kengine::functions::log
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_parents(
+		putils_reflection_type(refltype::base)
+	);
+	putils_reflection_used_types(
+		putils_reflection_type(kengine::log_event)
+	);
+};
+#undef refltype
+
+#define refltype kengine::log_event
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(severity),
+		putils_reflection_attribute(category),
+		putils_reflection_attribute(message)
+	);
+};
+#undef refltype
+
+#define refltype kengine::log_severity_control
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(severity)
+	);
+};
+#undef refltype
