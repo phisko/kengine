5b1bcac136ec5ea899357acad8751a2a9797d1fe
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-01-19 15:15:11 +0100

    physics: fix missing default value

--- kengine/data/physics.hpp
+++ kengine/data/physics.hpp
@@ -9,7 +9,7 @@ namespace kengine::data {
 	struct physics {
 		float mass = 1.f;
 
-		putils::vec3f movement;
+		putils::vec3f movement{ 0.f, 0.f, 0.f };
 		float yaw = 0.f; // radians to turn in frame
 		float pitch = 0.f; // radians to turn in frame
 		float roll = 0.f; // radians to turn in frame
