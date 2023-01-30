63d97bec6bd988107a78e8d67a09146efa08936b
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-01-30 08:12:29 +0100

    add pre_register_types

--- kengine/functions/register_types.hpp
+++ kengine/functions/register_types.hpp
@@ -8,8 +8,15 @@
 
 namespace kengine::functions {
 	using register_types_signature = void(entt::registry & r);
+
+	struct pre_register_types : base_function<register_types_signature> {};
+
 	struct register_types : base_function<register_types_signature> {};
 }
+
+#define refltype kengine::functions::pre_register_types
+kengine_function_reflection_info;
+#undef refltype
 
 #define refltype kengine::functions::register_types
 kengine_function_reflection_info;
