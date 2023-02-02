7fe548184f5bdcc7ebc8fa67bc43127baf87da80
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-02-02 16:02:54 +0100

    add KENGINE_NO_LOG option

--- kengine/helpers/log_helper.hpp
+++ kengine/helpers/log_helper.hpp
@@ -14,5 +14,10 @@ namespace kengine::log_helper {
 	KENGINE_CORE_EXPORT log_severity parse_command_line_severity(const entt::registry & r) noexcept;
 }
 
+#ifdef KENGINE_NO_LOG
+#define kengine_log(...) (void)0
+#define kengine_logf(...) (void)0
+#else
 #define kengine_log(registry, severity, category, message) kengine::log_helper::log(registry, kengine::log_severity::severity, category, message)
 #define kengine_logf(registry, severity, category, format, ...) kengine_log(registry, severity, category, putils::string<1024>(format, __VA_ARGS__).c_str())
+#endif
