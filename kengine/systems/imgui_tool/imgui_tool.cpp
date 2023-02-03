8e18c39186057c253aa2214a2e0de7230e961123
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-02-03 10:41:15 +0100

    imgui_tool: use backward_compatible_observer

--- kengine/systems/imgui_tool/imgui_tool.cpp
+++ kengine/systems/imgui_tool/imgui_tool.cpp
@@ -26,6 +26,7 @@
 
 // kengine helpers
 #include "kengine/helpers/assert_helper.hpp"
+#include "kengine/helpers/backward_compatible_observer.hpp"
 #include "kengine/helpers/log_helper.hpp"
 #include "kengine/helpers/profiling_helper.hpp"
 #include "kengine/helpers/sort_helper.hpp"
@@ -40,8 +41,42 @@
 
 namespace kengine::systems {
 	struct imgui_tool {
+		struct conf_file {
+			conf_file() noexcept {
+				parse();
+			}
+
+			void parse() noexcept {
+				KENGINE_PROFILING_SCOPE;
+				std::ifstream f(KENGINE_IMGUI_TOOLS_SAVE_FILE);
+				if (!f)
+					return;
+				for (std::string line; std::getline(f, line);)
+					add_line(line);
+			}
+
+			void add_line(const std::string & line) noexcept {
+				KENGINE_PROFILING_SCOPE;
+				const auto index = line.find(';');
+				_values[line.substr(0, index)] = putils::parse<bool>(line.substr(index + 1).c_str());
+			}
+
+			std::optional<bool> get_value(const char * name) const noexcept {
+				KENGINE_PROFILING_SCOPE;
+				const auto it = _values.find(name);
+				if (it == _values.end())
+					return std::nullopt;
+				return it->second;
+			}
+
+		private:
+			std::unordered_map<std::string, bool> _values;
+		};
+
+		conf_file configuration;
+
 		entt::registry & r;
-		putils::vector<entt::scoped_connection, 2> connections;
+		const entt::scoped_connection remove_tool = r.on_destroy<data::imgui_tool>().connect<&imgui_tool::on_destroy_imgui_tool>(this);
 
 		struct menu_entry {
 			using entry_map = std::map<std::string, menu_entry>;
@@ -55,25 +90,15 @@ namespace kengine::systems {
 			KENGINE_PROFILING_SCOPE;
 			kengine_log(r, log, "Init", "systems/imgui_tool");
 
-			conf_file.parse();
-			for (const auto & [tool_entity, name, tool] : r.view<data::name, data::imgui_tool>().each()) {
-				kengine_logf(r, log, "Init/systems/imgui_tool", "Initializing %s", name.name.c_str());
-				if (const auto value = conf_file.get_value(name.name.c_str()))
-					tool.enabled = *value;
-			}
-
 			e.emplace<functions::execute>(putils_forward_to_this(execute));
-
-			for (const auto tool_entity : r.view<data::imgui_tool>())
-				on_construct_imgui_tool(r, tool_entity);
-
-			connections.emplace_back(r.on_construct<data::imgui_tool>().connect<&imgui_tool::on_construct_imgui_tool>(this));
-			connections.emplace_back(r.on_destroy<data::imgui_tool>().connect<&imgui_tool::on_destroy_imgui_tool>(this));
 		}
 
+		kengine::backward_compatible_observer<data::imgui_tool> observer{ r, putils_forward_to_this(on_construct_imgui_tool) };
 		void execute(float delta_time) noexcept {
 			KENGINE_PROFILING_SCOPE;
 			kengine_log(r, verbose, "execute", "imgui_tool");
+
+			observer.process();
 
 			if (ImGui::BeginMainMenuBar()) {
 				bool must_save = false;
@@ -114,7 +139,7 @@ namespace kengine::systems {
 			}
 		}
 
-		void on_construct_imgui_tool(entt::registry & r, entt::entity e) noexcept {
+		void on_construct_imgui_tool(entt::entity e, data::imgui_tool & tool) noexcept {
 			KENGINE_PROFILING_SCOPE;
 
 			const auto name = r.try_get<data::name>(e);
@@ -123,9 +148,8 @@ namespace kengine::systems {
 				return;
 			}
 
-			auto & tool = r.get<data::imgui_tool>(e);
 			kengine_logf(r, log, "Init/systems/imgui_tool", "Initializing %s", name->name.c_str());
-			if (const auto value = conf_file.get_value(name->name.c_str()))
+			if (const auto value = configuration.get_value(name->name.c_str()))
 				tool.enabled = *value;
 
 			menu_entry * current_entry = &root_entry;
@@ -179,34 +203,6 @@ namespace kengine::systems {
 				f << name->name << ';' << std::boolalpha << tool->enabled << std::noboolalpha << std::endl;
 			f.flush();
 		}
-
-		struct {
-			void parse() noexcept {
-				KENGINE_PROFILING_SCOPE;
-				std::ifstream f(KENGINE_IMGUI_TOOLS_SAVE_FILE);
-				if (!f)
-					return;
-				for (std::string line; std::getline(f, line);)
-					add_line(line);
-			}
-
-			void add_line(const std::string & line) noexcept {
-				KENGINE_PROFILING_SCOPE;
-				const auto index = line.find(';');
-				_values[line.substr(0, index)] = putils::parse<bool>(line.substr(index + 1).c_str());
-			}
-
-			std::optional<bool> get_value(const char * name) const noexcept {
-				KENGINE_PROFILING_SCOPE;
-				const auto it = _values.find(name);
-				if (it == _values.end())
-					return std::nullopt;
-				return it->second;
-			}
-
-		private:
-			std::unordered_map<std::string, bool> _values;
-		} conf_file;
 	};
 
 	DEFINE_KENGINE_SYSTEM_CREATOR(imgui_tool)
