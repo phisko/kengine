9fa0742836a62e17057f10a31eb31cc645ed4c2e
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-02-03 11:57:38 +0100

    imgui_entity_selector: cache results

--- kengine/systems/imgui_entity_selector/imgui_entity_selector.cpp
+++ kengine/systems/imgui_entity_selector/imgui_entity_selector.cpp
@@ -36,6 +36,12 @@ namespace kengine::systems {
 		entt::registry & r;
 		bool * enabled;
 
+		struct search_result {
+			entt::entity e;
+			putils::string<1024> display_text;
+		};
+		std::vector<search_result> search_results;
+
 		imgui_entity_selector(entt::handle e) noexcept
 			: r(*e.registry()) {
 			KENGINE_PROFILING_SCOPE;
@@ -49,6 +55,7 @@ namespace kengine::systems {
 		}
 
 		char name_search[1024] = "";
+		bool search_out_of_date = true;
 		void execute(float delta_time) noexcept {
 			KENGINE_PROFILING_SCOPE;
 
@@ -57,78 +64,113 @@ namespace kengine::systems {
 			kengine_log(r, verbose, "execute", "imgui_entity_selector");
 
 			if (ImGui::Begin("Entity selector", enabled)) {
-				ImGui::InputText("Search", name_search, sizeof(name_search));
+				if (ImGui::InputText("Search", name_search, sizeof(name_search)))
+					search_out_of_date = true;
+
+				if (search_out_of_date) {
+					update_search_results();
+					search_out_of_date = false;
+				}
 
 				ImGui::Separator();
 
 				ImGui::BeginChild("child");
-				r.each([&](entt::entity e) {
-					if (matches(e, name_search)) {
-						if (r.all_of<data::selected>(e))
-							r.erase<data::selected>(e);
-						else
-							r.emplace<data::selected>(e);
-					}
-				});
+				display_search_results();
 				ImGui::EndChild();
 			}
 			ImGui::End();
 		}
 
-		bool matches(entt::entity e, const char * str) noexcept {
+		void update_search_results() noexcept {
 			KENGINE_PROFILING_SCOPE;
 
-			putils::string<1024> display_text("[%d]", e);
+			search_results.clear();
+			r.each([&](entt::entity e) {
+				apply_search(e);
+			});
+		}
+
+		void apply_search(entt::entity e) noexcept {
+			KENGINE_PROFILING_SCOPE;
+
+			search_result result {
+				e,
+				{ "[%zu]", e }
+			};
+
 			const auto name = r.try_get<data::name>(e);
 			if (name) {
-				display_text += " ";
-				display_text += name->name;
+				result.display_text += " ";
+				result.display_text += name->name;
 			}
 
-			if (strlen(str) != 0) {
-				display_text += " -- ";
+			if (strlen(name_search) != 0) {
+				result.display_text += " -- ";
 
 				bool matches = false;
-				if (str[0] >= '0' && str[0] <= '9' && putils::parse<entt::entity>(str) == e) {
+				if (std::isdigit(name_search[0]) && putils::parse<entt::entity>(name_search) == e) {
 					matches = true;
-					display_text += "ID";
+					result.display_text += "ID";
 				}
 				else {
 					const auto types = sort_helper::get_name_sorted_entities<const meta::has, const meta::match_string>(r);
 
 					for (const auto & [_, type, has, match_func] : types) {
-						if (!has->call({ r, e }) || !match_func->call({ r, e }, str))
+						if (!has->call({ r, e }) || !match_func->call({ r, e }, name_search))
 							continue;
 
-						if (display_text.size() + type->name.size() + 2 < decltype(display_text)::max_size) {
+						if (result.display_text.size() + type->name.size() + 2 < decltype(result.display_text)::max_size) {
 							if (matches) // Isn't the first time
-								display_text += ", ";
-							display_text += type->name;
+								result.display_text += ", ";
+							result.display_text += type->name;
 						}
 						matches = true;
 					}
 				}
 
 				if (!matches)
-					return false;
+					return;
 			}
 
+			search_results.push_back(result);
+		}
+
+		void display_search_results() noexcept {
+			KENGINE_PROFILING_SCOPE;
+
+			for (const auto & result : search_results)
+				if (display_search_result(result)) {
+					if (r.all_of<data::selected>(result.e))
+						r.erase<data::selected>(result.e);
+					else
+						r.emplace<data::selected>(result.e);
+				}
+		}
+
+		bool display_search_result(const search_result & result) noexcept {
+			KENGINE_PROFILING_SCOPE;
+
+			ImGui::PushID(int(result.e));
+
 			bool ret = false;
-			const auto open_tree_node = ImGui::TreeNode((display_text + "##" + int(e)).c_str());
+
+			const auto open_tree_node = ImGui::TreeNode(result.display_text.c_str());
 			if (ImGui::BeginPopupContextItem()) {
 				if (ImGui::MenuItem("Select"))
 					ret = true;
 				if (ImGui::MenuItem("Remove")) {
-					r.destroy(e);
+					r.destroy(result.e);
 					ret = false;
 				}
 				ImGui::EndPopup();
 			}
+
 			if (open_tree_node) {
-				imgui_helper::display_entity({ r, e });
+				imgui_helper::display_entity({ r, result.e });
 				ImGui::TreePop();
 			}
 
+			ImGui::PopID();
 			return ret;
 		}
 	};
