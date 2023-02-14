cab679205f084ce40f30e4171389a5d43e307905
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-02-14 14:27:04 +0100

    simplify system paths

--- kengine/log/log_imgui/systems/log_imgui.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// kengine system_creator
-#include "kengine/system_creator/helpers/system_creator_helper.hpp"
-
-namespace kengine::systems {
-	DECLARE_KENGINE_SYSTEM_CREATOR(KENGINE_LOG_LOG_IMGUI_EXPORT, log_imgui)
-}
