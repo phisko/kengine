ad35c6967948390fb846ade697d2af03f90588a3
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-02-28 09:52:44 +0100

    remove imgui_lua_bindings

--- kengine/scripting/lua/imgui_bindings/helpers/imgui_lua_helper.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// entt
-#include <entt/entity/fwd.hpp>
-
-namespace kengine::imgui_lua_helper {
-	KENGINE_SCRIPTING_LUA_IMGUI_BINDINGS_EXPORT void init_bindings(const entt::registry & r) noexcept;
-}
